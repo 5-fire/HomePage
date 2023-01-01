52ff3666e6253c965935856524e6d948058e6dfe
minh.vu <minhvu.box@gmail.com>
2023-01-01 11:32:33 +0700

    update info

--- js/data.js
+++ js/data.js
@@ -18,7 +18,7 @@ const currentYear = new Date().getFullYear();
 $(document).ready(function () {
     $('.fullname').html(data.fullName);
     $('.jobname').html(data.jobName);
-    $('.linkedin-link').attr('href', 'https://www.linkedin.com/in/duyminhvu');
+    $('.linkedin-link').attr('href', 'https://www.linkedin.com/in/hnimvu');
     $('.instagram-link').attr('href', 'https://www.instagram.com/hnim.uv/');
     $('.github-link').attr('href', 'https://github.com/mizuvu');
 
